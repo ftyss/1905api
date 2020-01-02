a691d2e2c8e7b2d0e9d38f444dead36b48e36c69
ftyss <1251898775@qq.com>
2020-01-02 00:35:35 +0000

    支付页面

---
+++ app/Http/Controllers/Alipay/PayController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Alipay;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PayController extends Controller
+{
+    //
+}
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,22 +8,24 @@ class TestController extends Controller
 {
     public function alipay()
     {
+        $ali_gateway = 'https://openapi.alipaydev.com/gateway.do';  //支付网关
         // 公共请求参数
         $appid = '2016101100657125';
         $method = 'alipay.trade.page.pay';
         $charset = 'utf-8';
         $signtype = 'RSA2';
         $sign = '';
-        $timestamp = date('Y-m-d H:i:s ');
+        $timestamp = date('Y-m-d H:i:s');
         $version = '1.0';
-        $notify_url = 'http://1905api.fangtaoys.com/alipay/notify';   //支付宝异步通知地址
+        $return_url = 'http://api1905.fangtaoys.com/test/alipay/return';       // 支付宝同步通知
+        $notify_url = 'http://api1905.fangtaoys.com/alipay/notify';   //支付宝异步通知地址
         $biz_content = '';
 
 
         // 请求参数
-        $out_trade_no = time() . rand(1111,9999);
-        $product_code = 'FAST_INSTENT_TRADE_PAY';
-        $total_amount = 0.01;
+        $out_trade_no = time() . rand(1111,9999);   //随机商户订单号
+        $product_code = 'FAST_INSTANT_TRADE_PAY';
+        $total_amount = 8888.88;
         $subject = '测试订单' . $out_trade_no;
 
 
@@ -42,12 +44,13 @@ class TestController extends Controller
             'sign_type' =>  $signtype,
             'timestamp' =>  $timestamp,
             'version'   =>  $version,
+            'return_url'=>  $return_url,
             'notify_url'=>  $notify_url,
             'biz_content'=> json_encode($request_param)
         ];
 
 
-        echo '<pre>';print_r($param);echo '</pre>';
+        //echo '<pre>';print_r($param);echo '</pre>';
 
 
         // 字典序排序
@@ -60,11 +63,32 @@ class TestController extends Controller
             $str .= $k . '=' . $v . '&';
         }
         //echo 'str：'.$str;die;
+        $str=rtrim($str,'&');
 
 
         // 3 计算签名
+        $key = storage_path('keys/app_priv');
+        $priKey = file_get_contents($key);
+        //dd($priKey);
+        $res = openssl_get_privatekey($priKey);
+        //var_dump($res);echo '</br>';die;
+        openssl_sign($str, $sign, $res, OPENSSL_ALGO_SHA256);
+        $sign = base64_encode($sign);
+        $param['sign'] = $sign;
+
+        // 4 urlencode
+        $param_str = '?';
+        foreach($param as $k=>$v){
+            $param_str .= $k.'='.urlencode($v) . '&';
+        }
+        $param_str = rtrim($param_str,'&');
+        $url = $ali_gateway . $param_str;
+        //发送GET请求
+        //echo $url;die;
+        header("Location:".$url);
 
 
-        $url = 'https://openapi.alipaydev.com/gateway.do?';
+
+        //$url = 'https://openapi.alipaydev.com/gateway.do?';
     }
 }
